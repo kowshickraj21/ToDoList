f4b1690099b04d64716cac85bfc2765c6ac1e564
kowshickraj21 <kowshickraj21.2005@gmail.com>
2023-08-12 12:08:22 +0530

    Third commit

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ function adding(){
     let myValue=document.getElementById("inpbox").value;
     document.getElementById("inpbox").value = "";
     let liel=document.createElement("li");
+    liel.className="liel";
+    let editor=document.createElement("button");
+    let delbtn =document.createElement("button");
+    let edit=document.createTextNode("Edit");
+    let del=document.createTextNode("Delete");
+    delbtn.className="delbtn";
+    editor.className="createbtn";
+    editor.appendChild(edit);
+    delbtn.appendChild(del);
     liel.appendChild(document.createTextNode(myValue));
+    liel.appendChild(editor);
+    liel.appendChild(delbtn);
     document.getElementById("list").appendChild(liel);
 }
+ function edit(){
+
+ }
